5ebb4d463f47a3a196f9c2bd897f5092604ab2a9
Pingu66 <melnyczukmarcin@gmail.com>
2022-03-28 19:58:47 +0200

    register

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php
 
-$db = new mysqli("localhost", "root","","med");
+$db = new mysqli("localhost", "root", "", "med");
 
 $q = $db->prepare("SELECT * FROM staff");
-
-if($q->execute()){
-    //ta czesc wykona sie jesli kwerenda bedzie poprwanie przetworzona przez serwer
+if($q && $q->execute()) {  
     $result = $q->get_result();
-    while($row = $result->fetch_assoc()){
-        $staff_id = $row['id'];
-        $firstName = $row['firstName'];
-        $lastName = $row['lastName'];
+    while($staff = $result->fetch_assoc()) {
+        $staffId = $staff['id'];
+        $firstName = $staff['firstName'];
+        $lastName = $staff['lastName'];
         echo "Lekarz $firstName $lastName<br>";
-        $q = $db->prepare("SELECT * FROM schedule WHERE staff_id = ?");
-        $q->bind_param("i", $staff_id);
-        if($q->execute()) {
-            $schedule = $q->get_result();
-            while($visit = $schedule->fetch_assoc()) {
-                $timestamp = strtotime($visit['date']);
-                echo "<button style=\"margin:10px;\">";
-                echo date("j/m/Y H:i", $timestamp);
-                echo "</button>";
+        $q = $db->prepare("SELECT * FROM appointment WHERE staff_id = ?");
+        $q->bind_param("i", $staffId);
+        if($q && $q->execute()) {
+            $appointments = $q->get_result();
+            while($appointment = $appointments->fetch_assoc()) {     
+                $appointmentId = $appointment['id'];
+                $appointmentDate = $appointment['date'];
+                $appointmentTimestamp = strtotime($appointmentDate);
+                echo "<a href=\"appointment.php?id=$appointmentId\" style=\"margin:10px; display:block\">";
+                echo date("j.m H:i", $appointmentTimestamp);
+                echo "</a>";
             }
-            
+            echo "<br>";
+        } else {
+            die("Błąd pobierania wizyt z bazy danych");
         }
-        echo "<br>";
     }
 } else {
-    echo "bład podczas wyszukiwania w bazie danych";
+    die("Błąd pobierania lekarzy z bazy danych");
 }
-
-
 
 ?>
--- med.sql
+++ med.sql
@@ -24,23 +24,64 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `schedule`
+-- Struktura tabeli dla tabeli `appointment`
 --
 
-CREATE TABLE `schedule` (
+CREATE TABLE `appointment` (
   `id` int(11) NOT NULL,
   `staff_id` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Zrzut danych tabeli `schedule`
+-- Zrzut danych tabeli `appointment`
 --
 
-INSERT INTO `schedule` (`id`, `staff_id`, `date`) VALUES
-(1, 1, '2022-03-23 12:00:00'),
-(2, 1, '2022-03-23 12:30:00'),
-(3, 2, '2022-03-23 10:00:00');
+INSERT INTO `appointment` (`id`, `staff_id`, `date`) VALUES
+(1, 1, '2022-03-24 12:00:00'),
+(2, 1, '2022-03-24 14:00:00'),
+(3, 2, '2022-03-24 13:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `phone` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `patient`
+--
+
+INSERT INTO `patient` (`id`, `firstName`, `lastName`, `phone`) VALUES
+(1, 'Stefek', 'Burczymucha', '+48666666666'),
+(2, 'Edwin', 'Piatek', '+48789456123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `patientappointment`
+--
+
+CREATE TABLE `patientappointment` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `appointment_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `patientappointment`
+--
+
+INSERT INTO `patientappointment` (`id`, `patient_id`, `appointment_id`) VALUES
+(1, 1, 1),
+(2, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -59,7 +100,7 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`id`, `firstName`, `lastName`) VALUES
-(1, 'Jan ', 'Kowalski'),
+(1, 'Jan', 'Kowalski'),
 (2, 'Adam', 'Nowak'),
 (3, 'Iwona', 'Tabletka');
 
@@ -68,11 +109,24 @@ INSERT INTO `staff` (`id`, `firstName`, `lastName`) VALUES
 --
 
 --
--- Indeksy dla tabeli `schedule`
+-- Indeksy dla tabeli `appointment`
 --
-ALTER TABLE `schedule`
+ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`),
   ADD KEY `staff_id` (`staff_id`);
+
+--
+-- Indeksy dla tabeli `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `patientappointment`
+--
+ALTER TABLE `patientappointment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `appointment_id` (`appointment_id`);
 
 --
 -- Indeksy dla tabeli `staff`
@@ -85,10 +139,22 @@ ALTER TABLE `staff`
 --
 
 --
--- AUTO_INCREMENT dla tabeli `schedule`
+-- AUTO_INCREMENT dla tabeli `appointment`
 --
-ALTER TABLE `schedule`
+ALTER TABLE `appointment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT dla tabeli `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `patientappointment`
+--
+ALTER TABLE `patientappointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT dla tabeli `staff`
@@ -101,10 +167,10 @@ ALTER TABLE `staff`
 --
 
 --
--- Ograniczenia dla tabeli `schedule`
+-- Ograniczenia dla tabeli `appointment`
 --
-ALTER TABLE `schedule`
-  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
